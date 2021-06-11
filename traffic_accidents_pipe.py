f9a26a248c4bb14f64cf63d7daee7238c35e0abb
z <aerodinamicbg@gmail.com>
2021-06-11 22:15:46 +0300

    turn into a class

--- traffic_accidents_pipe.py
+++ traffic_accidents_pipe.py
@@ -3,124 +3,165 @@ import os
 import psycopg2
 import yaml
 from io import StringIO
-
+from kaggle.api.kaggle_api_extended import KaggleApi
+from zipfile import ZipFile
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
-_WORKING_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 
-with open(os.path.join(_WORKING_DIRECTORY, 'config.yaml')) as file:
-    _CONFIG = yaml.full_load(file)
+class Pipeline():
+    def __init__(self):
+        self._WORKING_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 
-
-def read_raw_data():
-    """
-        Fetches data from kaggle.
-        Filters out accidents we don't have vehicles information for.
-        Joins accidents + vehicles dataframes. 
-    """
-    os.system('kaggle datasets download -d tsiaras/uk-road-safety-accidents-and-vehicles -w --unzip -p \"{}\"'.format(_WORKING_DIRECTORY))
-
-    acc = pd.read_csv(os.path.join(_WORKING_DIRECTORY, _CONFIG['accidents_csv']), usecols=_CONFIG['list_of_acc_columns'], dtype={'Accident_Index': str})
-    acc.columns = acc.columns.str.lower()
-    veh = pd.read_csv(os.path.join(_WORKING_DIRECTORY, _CONFIG['vehicles_csv']), usecols=_CONFIG['list_of_veh_columns'], dtype={'Accident_Index': str})
-    veh.columns = veh.columns.str.lower()
-
-    acc = acc[acc['accident_index'].isin(veh['accident_index'])]
-    acc.sort_values(by=['date', 'time'], inplace=True)
-    acc.drop(columns=['time'], inplace=True)
-
-    is_take_subset = bool(_CONFIG['is_take_only_100_recent_accidents'])
-    acc_subset = acc[-100:] if is_take_subset else acc
-
-    merged = pd.merge(acc_subset, veh, on='accident_index', how='inner')
-
-    return merged
+        with open(os.path.join(self._WORKING_DIRECTORY, 'config.yaml')) as file:
+            self._CONFIG = yaml.full_load(file)
 
 
-def create_db():
-    """
-        Checks if a database with the specified name already exists. 
-        If yes - it skips the creation.
-        If no - creates the database.
-    """
-    with psycopg2.connect(host=_CONFIG['db_host'], \
-                        port=_CONFIG['db_port'], \
-                        user=_CONFIG['db_user'], \
-                        password=_CONFIG['db_pass']) as conn:
-        conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    def download_data(self):
+        """
+            Checks if files are pre-downloaded and if not fetches them through the Kaggle API.
+        """
+        dir_list = os.listdir(self._WORKING_DIRECTORY)
+        if self._CONFIG['accidents_csv'] not in dir_list or self._CONFIG['vehicles_csv'] not in dir_list:
+            # check that api.dataset_download_files does not get called if zipped_dataset is in dir.
+            # and the opposite
+            if self._CONFIG['zipped_dataset'] not in dir_list:
+                api = KaggleApi()
+                api.authenticate()
+
+                api.dataset_download_files(self._CONFIG['kaggle_dataset'], \
+                                            path=_WORKING_DIRECTORY)
+
+            with ZipFile(os.path.join(self._WORKING_DIRECTORY, self._CONFIG['zipped_dataset'])) as zip_file:
+                    zip_file.extractall(self._WORKING_DIRECTORY)
+
+
+    def read_in_csv(self, file, columns):
+        """
+            Reads in a pandas dataframe and lowercases its columns.
+        """
+        source_df = pd.read_csv(os.path.join(self._WORKING_DIRECTORY, file), usecols=columns, dtype={'Accident_Index': str})
+        source_df.columns = source_df.columns.str.lower()
+
+        return source_df
+
+
+    def merge_data(self, acc, veh):
+        """
+            Filters out accidents we don't have vehicles information for.
+            Joins accidents + vehicles dataframes. 
+        """
+        acc = acc[acc['accident_index'].isin(veh['accident_index'])]
+        acc.sort_values(by=['date', 'time'], inplace=True)
+        acc.drop(columns=['time'], inplace=True)
+
+        is_take_subset = bool(self._CONFIG['is_take_only_100_recent_accidents'])
+        acc_subset = acc[-100:] if is_take_subset else acc
+
+        merged = pd.merge(acc_subset, veh, on='accident_index', how='inner')
+        # test that merged is 8 columns and 170 rows
+        return merged
+
+
+    def create_db(self):
+        """
+            Checks if a database with the specified name already exists. 
+            If yes - it skips the creation.
+            If no - creates the database.
+        """
+        with psycopg2.connect(host=self._CONFIG['db_host'], \
+                            port=self._CONFIG['db_port'], \
+                            user=self._CONFIG['db_user'], \
+                            password=self._CONFIG['db_pass']) as conn:
+            conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+            with conn.cursor() as cursor:
+                cursor.execute("SELECT 1 FROM pg_catalog.pg_database WHERE datname = '{}'".format(self._CONFIG['db_name']))
+                is_exist = cursor.fetchone()
+                if not is_exist:
+                    cursor.execute("create database {} owner {}".format(self._CONFIG['db_name'], self._CONFIG['db_user']))
+                # check that cursor is called once if is_exist is not None and twice if is_exist is None
+
+
+    def deploy_schema(self, conn):
+        """
+            Deploys a predefined schema.
+        """
         with conn.cursor() as cursor:
-            cursor.execute("SELECT 1 FROM pg_database WHERE datname = '{}'".format(_CONFIG['db_name']))
-            is_exist = cursor.fetchone()
-            if not is_exist:
-                cursor.execute("create database {} owner {}".format(_CONFIG['db_name'], _CONFIG['db_user']))
+            with open(os.path.join(self._WORKING_DIRECTORY, self._CONFIG['db_schema'])) as schema:
+                cursor.execute(schema.read())
+                conn.commit()
+                #check whether connection commit is called once
 
 
-def deploy_schema(conn):
-    """
-        Deploys a predefined schema.
-    """
-    with conn.cursor() as cursor:
-        with open(os.path.join(_WORKING_DIRECTORY, _CONFIG['db_schema'])) as schema:
-            cursor.execute(schema.read())
-            conn.commit()
+    def lookup_values_in_db(self, conn, df):
+        """
+            The fact table holds foreign keys to a number of dimention tables. 
+            This function looks up the dimention table ids.
+            With every iteration we add a new *_id column to our frame, while dropping the string representation of the field.
+        """
+        with conn.cursor() as cursor:
+            for dim in self._CONFIG['dimension_lookups']:
+                dim_table = 'dim_' + dim
+
+                cursor.execute('SELECT * FROM {}'.format(dim_table))
+                colnames = [desc[0] for desc in cursor.description]
+                lookup_df = pd.DataFrame(cursor.fetchall(), columns=colnames)
+
+                df = pd.merge(df,
+                            lookup_df,
+                            how='left',
+                            on=dim)
+                df.drop(columns=[dim], inplace=True)
+        # test that merged is 8 columns and 170 rows
+        # check that *_id columns = len(self._CONFIG['dimension_lookups'])
+        return df
 
 
-def lookup_values_in_db(conn, df):
-    """
-        The fact table holds foreign keys to a number of dimention tables. 
-        This function looks up the dimention table ids.
-        With every iteration we add a new *_id column to our frame, while dropping the string representation of the field.
-    """
-    with conn.cursor() as cursor:
-        for dim in _CONFIG['dimension_lookups']:
-            dim_table = 'dim_' + dim
-
-            cursor.execute('SELECT * FROM {}'.format(dim_table))
-            colnames = [desc[0] for desc in cursor.description]
-            lookup_df = pd.DataFrame(cursor.fetchall(), columns=colnames)
-
-            df = pd.merge(df,
-                        lookup_df,
-                        how='left',
-                        on=dim)
-            df.drop(columns=[dim], inplace=True)
-
-    return df
+    def fetch_columns_order_from_fact_table(self, conn):
+        """
+            Used for fetching columns order from the fact table.
+        """
+        with conn.cursor() as cursor:
+            cursor.execute('SELECT * FROM {} LIMIT 1'.format(self._CONFIG['fact_table']))
+            
+            return [desc[0] for desc in cursor.description]
 
 
-def copy_to_db(conn, df):
-    """
-        Checks for the order of the dataframe columns and inserts it to the fact table.
-    """
-    with conn.cursor() as cursor:
-        cursor.execute('SELECT * FROM {} LIMIT 1'.format(_CONFIG['fact_table']))
-        colnames = [desc[0] for desc in cursor.description]
-        df = df[colnames]
-
-        buffer = StringIO()
-        df.to_csv(buffer, index=False, header=False)
-        buffer.seek(0)
-        try:
-            cursor.copy_from(buffer, _CONFIG['fact_table'], null="", sep=',')
-            conn.commit()
-        except Exception as error:
-            print("Error: {}".format(error)) 
-            conn.rollback()
+    def copy_to_db(self, conn, df):
+        """
+            Inserts dataframe to the fact table.
+        """
+            buffer = StringIO()
+            df.to_csv(buffer, index=False, header=False)
+            buffer.seek(0)
+            try:
+                cursor.copy_from(buffer, self._CONFIG['fact_table'], null="", sep=',')
+                #check whether connection commit is called once
+                conn.commit()
+            except Exception as error:
+                #simulate side effects
+                print("Error: {}".format(error)) 
+                conn.rollback()
 
 
-def main():
-    df = read_raw_data()
-    create_db()
+    def run(self):
+        self.download_data()
+        acc = self.read_in_csv(self._CONFIG['accidents_csv'], self._CONFIG['list_of_acc_columns'])
+        veh = self.read_in_csv(self._CONFIG['vehicles_csv'], self._CONFIG['list_of_veh_columns'])
+        df = self.merge_data(acc, veh)
 
-    with psycopg2.connect(host=_CONFIG['db_host'], \
-                        database=_CONFIG['db_name'], \
-                        port=_CONFIG['db_port'], \
-                        user=_CONFIG['db_user'], \
-                        password=_CONFIG['db_pass']) as conn:
-        deploy_schema(conn)
-        df = lookup_values_in_db(conn, df)
-        copy_to_db(conn, df)
+        self.create_db()
 
+        with psycopg2.connect(host=self._CONFIG['db_host'], \
+                            database=self._CONFIG['db_name'], \
+                            port=self._CONFIG['db_port'], \
+                            user=self._CONFIG['db_user'], \
+                            password=self._CONFIG['db_pass']) as conn:
+            self.deploy_schema(conn)
+            df = self.lookup_values_in_db(conn, df)
+            df = df[self.fetch_columns_order_as_per_db(conn)]
+            self.copy_to_db(conn, df)
 
+ 
 if __name__ == '__main__':
-    main()
+    pipe = Pipeline()
+    pipe.run()
